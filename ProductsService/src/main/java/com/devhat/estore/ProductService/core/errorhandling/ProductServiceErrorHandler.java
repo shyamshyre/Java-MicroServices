e3c9ca166980d546a1ebadac8f484f9b888c097a
shyamshyre <shyam.shyre@gmail.com>
2021-05-27 17:14:51 +0530

    Added Global Exception Handling..
    Added Global Exception Handling..

--- ProductsService/src/main/java/com/devhat/estore/ProductService/core/errorhandling/ProductServiceErrorHandler.java
+++ ProductsService/src/main/java/com/devhat/estore/ProductService/core/errorhandling/ProductServiceErrorHandler.java
@@ -1,5 +1,7 @@
 package com.devhat.estore.ProductService.core.errorhandling;
 
+import java.util.Date;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,13 +15,15 @@ public class ProductServiceErrorHandler {
 	// specify the specific exception you want to handle
 	@ExceptionHandler(value= {IllegalStateException.class})
 	public ResponseEntity<Object> handleIllegalStateException(IllegalStateException ex, WebRequest request){
-		return new ResponseEntity<>(ex.getMessage(), new HttpHeaders(),HttpStatus.INTERNAL_SERVER_ERROR);
+		ErrorMessage  errorMessage = new ErrorMessage(new Date(), ex.getMessage());
+		return new ResponseEntity<>(errorMessage, new HttpHeaders(),HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
 	
 	@ExceptionHandler(value= {Exception.class})
 	public ResponseEntity<Object> handleOtherException(Exception ex, WebRequest request){
-		return new ResponseEntity<>(ex.getMessage(), new HttpHeaders(),HttpStatus.INTERNAL_SERVER_ERROR);
+		ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
+		return new ResponseEntity<>(errorMessage, new HttpHeaders(),HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
 }
